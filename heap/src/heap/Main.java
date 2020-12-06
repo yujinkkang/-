fe2a982a25a9b5d7e5e8f4e185fcb9e3e69d8f31
yujinkkang <misoti1012@gmail.com>
2020-12-06 19:24:21 +0900

    heap 알고리즘 정리

---
+++ heap/src/heap/Main.java
@@ -0,0 +1,12 @@
+package heap;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int[] scoville = {1, 2, 3, 9, 10, 12};
+		int K = 7;
+		MoreSpicy_answer e = new MoreSpicy_answer();
+		System.out.println("¼¯Àº È½¼ö: " + e.solution(scoville, K));
+	}
+
+}
