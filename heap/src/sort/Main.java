721970e9a0e003f2e7e501438efca693c085a19b
yujinkkang <misoti1012@gmail.com>
2020-12-10 18:41:50 +0900

    정렬알고리즘

---
+++ heap/src/sort/Main.java
@@ -0,0 +1,17 @@
+package sort;
+
+public class Main {
+
+   public static void main(String[] args) {
+	   
+	   //int[] numbers = {3, 30, 34, 5, 9};
+	   
+	   //BiggestNumber bn = new BiggestNumber();
+	   //bn.solution(numbers);
+	   
+	   //System.out.println("´ä: "+ bn.solution(numbers));
+
+
+   }
+
+}
